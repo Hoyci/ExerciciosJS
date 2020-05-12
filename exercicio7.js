371bf940b27027f3108ec0c41828150c5a41f628
Hoyci <ruan.pablo.pec@github.com>
2020-05-11 23:52:21 -0300

    Agora sim

--- exercicio7.js
+++ exercicio7.js
@@ -1,19 +1,17 @@
 function bhaskara(a, b, c) {
-    if (delta = (b**2 - 4 * a * c) > 0) {
-        console.log("Delta é positivo")
-        const x1 = (-b + Math.sqrt(delta)) / 2 * a
-        const x2 = (-b - Math.sqrt(delta)) / 2 * a
-        console.log("x1 =", x1,"e", "x2 =", x2)
-    } else if (delta = b**2 - 4 * a * c == 0) {
-        console.log("Delta é nulo")
-        const x1 = (-b + Math.sqrt(delta)) / 2 * a
-        const x2 = (-b - Math.sqrt(delta)) / 2 * a
-        console.log("x1 =", x1, "e", "x2 =", x2)
+    if ((delta = b**2 - 4 * a * c) > 0)  {
+        const x1 = ((-b + Math.sqrt(delta)) / 2 * a)
+        const x2 = ((-b - Math.sqrt(delta)) / 2 * a)
+        console.log(x1, x2)
+    } else if ((delta = b**2 - 4 * a * c) == 0) {
+        const x1 = ((-b + Math.sqrt(delta)) / 2 * a)
+        const x2 = ((-b - Math.sqrt(delta)) / 2 * a)
+        console.log(x1, x2)
     } else {
-        console.log("Delta é negativo, logo não possui valores reais")
+        console.log("Delta é negativo")
     }
-}
+} 
 
 bhaskara(1, 12, -13) // Delta 
 bhaskara(4, -4, 1) // Delta nulo
-bhaskara(1, -4, 5) // Delta negativo
+bhaskara(1, -4, 5) // Delta Negativo
